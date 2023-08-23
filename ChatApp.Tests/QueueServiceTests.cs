7065ea9d15bafc5aaed0be98398a8a1ad5aa2597
Martin Strapko <martin.strapko@icloud.com>
2023-08-23 20:48:29 +0200

    Overflow functionality for QueueService

--- ChatApp.Tests/QueueServiceTests.cs
+++ ChatApp.Tests/QueueServiceTests.cs
@@ -32,21 +32,6 @@ namespace ChatApp.Tests
         }
 
         [Fact]
-        public void Dequeue_ShouldRemoveAndReturnChatSessionFromFront()
-        {
-            var chatSession1 = new ChatSession { SessionId = Guid.NewGuid() };
-            var chatSession2 = new ChatSession { SessionId = Guid.NewGuid() };
-
-            _queueService.TryEnqueue(chatSession1);
-            _queueService.TryEnqueue(chatSession2);
-
-            var dequeuedSession = _queueService.Dequeue();
-
-            Assert.Equal(chatSession1.SessionId, dequeuedSession?.SessionId);
-            Assert.False(_queueService.ContainsSession(chatSession1.SessionId));
-        }
-
-        [Fact]
         public void InitiateChat_WhenQueueIsFull_ShouldReturnBadRequest()
         {
             var mockQueueService = new Mock<IQueueService>();
@@ -81,6 +66,139 @@ namespace ChatApp.Tests
             var result = _queueService.TryEnqueue(anotherChatSession);
 
             Assert.False(result);
+        }
+
+        [Fact]
+        public void TryEnqueue_ShouldAddToOverflow_WhenMainQueueIsFullAndItsOfficeHours()
+        {
+            _queueService.MaxCapacity = 1;
+            _queueService.OverflowCapacity = 1;
+            var currentTime = DateTime.UtcNow;
+            _queueService.OfficeStart = currentTime.TimeOfDay - TimeSpan.FromHours(1);  // Office started 1 hour ago
+            _queueService.OfficeEnd = currentTime.TimeOfDay + TimeSpan.FromHours(1);    // Office ends in 1 hour
+
+
+            _queueService.TryEnqueue(new ChatSession());
+
+            var anotherChatSession = new ChatSession();
+            var result = _queueService.TryEnqueue(anotherChatSession);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TryEnqueue_ShouldNotAddToOverflow_WhenOutsideOfficeHours()
+        {
+            _queueService.MaxCapacity = 1;
+            _queueService.OverflowCapacity = 1;
+            var currentTime = DateTime.UtcNow;
+            _queueService.OfficeStart = currentTime.TimeOfDay + TimeSpan.FromMinutes(10);  // Office starts in 10 minutes
+            _queueService.OfficeEnd = currentTime.TimeOfDay + TimeSpan.FromHours(8);      // Office ends in 8 hours
+
+            _queueService.TryEnqueue(new ChatSession());
+
+            var anotherChatSession = new ChatSession();
+            var result = _queueService.TryEnqueue(anotherChatSession);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TryEnqueue_ShouldRefuseChat_WhenBothQueuesAreFull()
+        {
+            _queueService.MaxCapacity = 1;
+            _queueService.OverflowCapacity = 1;
+            _queueService.TryEnqueue(new ChatSession());
+            _queueService.TryEnqueue(new ChatSession());
+
+            var thirdChatSession = new ChatSession();
+            var result = _queueService.TryEnqueue(thirdChatSession);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Dequeue_ShouldRemoveAndReturnChatSessionFromFront()
+        {
+            var chatSession1 = new ChatSession();
+            var chatSession2 = new ChatSession();
+
+            _queueService.TryEnqueue(chatSession1);
+            _queueService.TryEnqueue(chatSession2);
+
+            var dequeuedSession = _queueService.Dequeue();
+
+            Assert.Equal(chatSession1.SessionId, dequeuedSession?.SessionId);
+            Assert.False(_queueService.ContainsSession(chatSession1.SessionId));
+        }
+
+        [Fact]
+        public void Dequeue_BothQueuesEmpty_ReturnsNull()
+        {
+            var queueService = new QueueService();
+
+            var result = queueService.Dequeue();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Dequeue_MainQueueHasItems_ReturnsItemFromMainQueue()
+        {
+            var queueService = new QueueService();
+            var chatSession = new ChatSession();
+            queueService.TryEnqueue(chatSession);
+
+            var result = queueService.Dequeue();
+
+            Assert.Equal(chatSession, result);
+        }
+
+        [Fact]
+        public void Dequeue_MainAndOverflowBothHaveItems_ReturnsItemFromMainQueueFirst()
+        {
+            _queueService.MaxCapacity = 5;
+            _queueService.OverflowCapacity = 1;
+            var currentTime = DateTime.UtcNow;
+            _queueService.OfficeStart = currentTime.TimeOfDay - TimeSpan.FromHours(1);
+            _queueService.OfficeEnd = currentTime.TimeOfDay + TimeSpan.FromHours(1);
+
+            var firstInQueue = new ChatSession();
+            _queueService.TryEnqueue(firstInQueue);
+
+            for (int i = 0; i < _queueService.MaxCapacity; i++)
+            {
+                _queueService.TryEnqueue(new ChatSession());
+            }
+
+            var resultFromMain = _queueService.Dequeue();
+
+            Assert.NotNull(resultFromMain);
+            Assert.Equal(firstInQueue, resultFromMain);
+        }
+
+        [Fact]
+        public void Dequeue_MainQueueEmptyOverflowHasItems_ReturnsItemFromOverflow()
+        {
+            _queueService.MaxCapacity = 10;
+            _queueService.OverflowCapacity = 1;
+            var currentTime = DateTime.UtcNow;
+            _queueService.OfficeStart = currentTime.TimeOfDay - TimeSpan.FromHours(1);
+            _queueService.OfficeEnd = currentTime.TimeOfDay + TimeSpan.FromHours(1);
+            for (int i = 0; i < 11; i++)
+            {
+                _queueService.TryEnqueue(new ChatSession());
+            }
+
+            // Dequeue all items from main queue
+            for (int i = 0; i < 10; i++)
+            {
+                _queueService.Dequeue();
+            }
+
+            var overflowSession = _queueService.Dequeue();
+
+            Assert.NotNull(overflowSession);
         }
     }
 }
--- ChatApp/Services/QueueService.cs
+++ ChatApp/Services/QueueService.cs
@@ -4,7 +4,11 @@ using ChatApp.Models;
 public class QueueService : IQueueService
 {
     private readonly Queue<ChatSession> _queue = new();
+    private readonly Queue<ChatSession> _overflowQueue = new();
     public int MaxCapacity { get; set; } = 10;
+    public int OverflowCapacity { get; set; } = 10;
+    public TimeSpan OfficeStart { get; set; } = new TimeSpan(9, 0, 0);
+    public TimeSpan OfficeEnd { get; set; } = new TimeSpan(17, 0, 0);
 
     public bool TryEnqueue(ChatSession session)
     {
@@ -15,19 +19,29 @@ public class QueueService : IQueueService
             _queue.Enqueue(session);
             return true;
         }
+        else if (IsOfficeHours() && _overflowQueue.Count < OverflowCapacity)
+        {
+            _overflowQueue.Enqueue(session);
+            return true;
+        }
         return false;
     }
 
     public ChatSession? Dequeue()
     {
-        if (_queue.Count == 0)
-            return null;
-
-        return _queue.Dequeue();
+        return _queue.Count > 0 ? _queue.Dequeue() :
+               _overflowQueue.Count > 0 ? _overflowQueue.Dequeue() :
+               null;
     }
 
     public bool ContainsSession(Guid sessionId)
     {
         return _queue.Any(s => s.SessionId == sessionId);
+    }
+
+    private bool IsOfficeHours()
+    {
+        var now = DateTime.UtcNow.TimeOfDay;
+        return now > OfficeStart && now < OfficeEnd;
     }
 }
