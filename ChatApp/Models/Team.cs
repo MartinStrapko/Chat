93a44382dd2f9d01a59c06e44d33657b6b06b998
Martin Strapko <martin.strapko@icloud.com>
2023-08-23 23:38:53 +0200

    Add Team model

---
+++ ChatApp/Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Agent> Agents { get; set; }
+    }
+}
