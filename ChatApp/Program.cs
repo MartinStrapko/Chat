a0c8a777e45cdb73a9913a3d0189c43c7e6866a1
Martin Strapko <martin.strapko@icloud.com>
2023-08-23 16:20:31 +0200

    Setup middleware to enable controller functionality

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
