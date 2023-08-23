01f1dfbcf826ff01b8c5ea40961de740523fc0f1
Martin Strapko <martin.strapko@icloud.com>
2023-08-23 16:12:07 +0200

    Added basic ChatController and test for it

--- ChatApp.Tests/UnitTest1.cs
+++ ChatApp.Tests/UnitTest1.cs
@@ -1,10 +1,17 @@
+using ChatApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
 namespace ChatApp.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CreateChat_ShouldReturnOkResult()
     {
+        var controller = new ChatController();
 
+        var result = controller.InitiateChat();
+
+        Assert.IsType<OkResult>(result);
     }
 }
---
+++ ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+        [HttpPost("create-session")]
+        public IActionResult InitiateChat()
+        {
+            return Ok();
+        }
+    }
+}
