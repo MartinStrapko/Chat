3905f231c1d41ef2689b2d8897010304f91c2df6
Martin Strapko <martin.strapko@icloud.com>
2023-08-23 18:38:50 +0200

    Added interface for the queue service

---
+++ ChatApp/Interfaces/IQueueService.cs
@@ -0,0 +1,16 @@
+﻿using ChatApp.Models;
+
+namespace ChatApp.Interfaces
+{
+    public interface IQueueService
+    {
+        // Try to add a chat session to the queue
+        bool TryEnqueue(ChatSession session);
+
+        // Remove a chat session from the queue
+        ChatSession? Dequeue();
+
+        // Checks if a session is already in the queue
+        bool ContainsSession(Guid sessionId);
+    }
+}
