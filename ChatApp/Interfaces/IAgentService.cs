a703e4fbda29d59c41948403ffaa8d053c4df0ea
Martin Strapko <martin.strapko@icloud.com>
2023-08-30 15:28:48 +0200

    OnChatEnded was missing on interface

--- ChatApp/Interfaces/IAgentService.cs
+++ ChatApp/Interfaces/IAgentService.cs
@@ -4,6 +4,7 @@ namespace ChatApp.Interfaces
 {
     public interface IAgentService
     {
+        event Action OnChatEnded;
         Agent? AssignChatToAgent(ChatSession chatSession);
         Agent? GetAvailableAgent();
         bool EndChat(Guid sessionId);
